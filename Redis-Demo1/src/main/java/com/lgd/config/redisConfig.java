ebde0266950a291bbe3e221d1bffecd9cf12e853
lgd <1017276522@qq.com>
2022-05-06 16:16:31 +0800

    hash and pipeline study.

---
+++ Redis-Demo1/src/main/java/com/lgd/config/redisConfig.java
@@ -0,0 +1,39 @@
+package com.lgd.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+/**
+ * @Description:
+ * @author: LGD
+ * @date:2022/5/6 10:22
+ */
+@Configuration
+public class redisConfig {
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        template.setConnectionFactory(factory);
+        //key序列化方式
+        template.setKeySerializer(redisSerializer);
+        //value序列化
+        template.setValueSerializer(redisSerializer);
+        //value hashmap序列化
+        template.setHashValueSerializer(redisSerializer);
+        //key haspmap序列化
+        template.setHashKeySerializer(redisSerializer);
+        return template;
+    }
+}
--- Redis-Demo1/src/test/java/com/lgd/RedisDemo1ApplicationTests.java
+++ Redis-Demo1/src/test/java/com/lgd/RedisDemo1ApplicationTests.java
@@ -3,7 +3,18 @@ package com.lgd;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StopWatch;
+
+import java.util.*;
 
 @SpringBootTest
 class RedisDemo1ApplicationTests {
@@ -13,8 +24,54 @@ class RedisDemo1ApplicationTests {
 
     @Test
     void contextLoads() {
-        redisTemplate.opsForValue().set("lgd","Hello!World!!");
-        System.out.println(redisTemplate.opsForValue().get("lgd"));
+
+        Set<String> keys = redisTemplate.keys("*");
+        redisTemplate.delete(keys);
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("使用pipeline");
+
+        long startTime = System.currentTimeMillis();
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        List list = redisTemplate.executePipelined(new RedisCallback<Object>() {
+            @Override
+            public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                for (int i = 0; i < 100000; i++) {
+                    String key = "测试" + String.valueOf(i);
+                    redisConnection.hSet(stringRedisSerializer.serialize("lalala1"),stringRedisSerializer.serialize(key),stringRedisSerializer.serialize("666666"));
+                }
+                return null;
+            }
+        },stringRedisSerializer);
+
+        long endTime = System.currentTimeMillis();
+        long costTime = endTime - startTime;
+        System.out.println(costTime);
+        stopWatch.stop();
+        Set<String> keys1 = redisTemplate.keys("*");
+        redisTemplate.delete(keys1);
+        stopWatch.start("不使用pipeline");
+        long startTime2 = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            String key = "测试" + String.valueOf(i);
+            redisTemplate.opsForHash().put("lalala2",key,"666666");
+        }
+        long endTime2 = System.currentTimeMillis();
+        long costTime2 = endTime2 - startTime2;
+        System.out.println(costTime2);
+        stopWatch.stop();
+
+
+//        redisTemplate.opsForHash().put("map1", "k1", "value1");
+//        redisTemplate.opsForHash().put("map1", "k2", "value2");
+//        //获取map
+//        Map map1 = redisTemplate.opsForHash().entries("map1");
+//        System.out.println(map1.get("k1"));
+//
+//        System.out.println(redisTemplate.opsForHash().get("map1","k2"));
+
+
+//        redisTemplate.opsForValue().set("lgd","Hello!World!!");
+//        System.out.println(redisTemplate.opsForValue().get("lgd"));
 //        redisTemplate.delete("lgd");
 //        System.out.println(redisTemplate.opsForValue().get("lgd"));
     }
